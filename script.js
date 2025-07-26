b18521c3abe00a9fd551fe17d4b0e4c309116515
Yanand666 <156229672+Yanand666@users.noreply.github.com>
2025-07-26 18:56:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,9 @@ window.onload = () => {
     });
   });
 };
+function addToCart(name, price) {
+  const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+  cart.push({ name, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(`Товар "${name}" додано до кошика!`);
+}
